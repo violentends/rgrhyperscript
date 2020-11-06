2604284b1aab4ec5af9b0043f31f4353f39135aa
vi <vi@nzmc.co>
2020-11-06 17:53:46 -0600

    adding copy to clipboard btn

--- main.js
+++ main.js
@@ -14,6 +14,18 @@ function hypers([...args]) {
   return topString + bottomString;
 }
 
+function copyToClipboard(copyText) {
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+}
+
+
 let $ = document.querySelector.bind(document);
 function getFormValues(formEl) {
   return formEl.$$("input").map((el) => el.value);
@@ -27,6 +39,10 @@ Node.prototype.on = function (ev, cb) {
 
 let form = $("form");
 let output = $("pre");
+let clipBtn = $('#copyClip');
+clipBtn.on('click', _ => {
+   copyToClipboard(output);
+})
 form.on("submit", (e) => {
   e.preventDefault();
   let string = hypers(getFormValues(e.target));
