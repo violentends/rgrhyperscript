9cb816844eb4a5754a1069699ec958bbe6282def
vi <vi@nzmc.co>
2020-11-06 18:12:57 -0600

    fixing copy to clipboard btn

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 function hypers([...args]) {
   let string = args.join(" ");
-  let topString = " \n";
-  let bottomString = "\n";
+  let topString = " \r\n";
+  let bottomString = "\r\n";
   for (let i = 0; i < string.length; i++) {
     if (!/[a-zA-Z]/.test(string.charAt(i))) {
       topString += "\t";
       bottomString += "\t";
     } else {
-      topString += " :regional_indicator_" + string.charAt(i) + ": ";
+      topString +=
+        " :regional_indicator_" + string.charAt(i).toLowerCase() + ": ";
       bottomString += " :RGRhype: ";
     }
   }
@@ -16,15 +17,19 @@ function hypers([...args]) {
 
 function copyToClipboard(copyText) {
   /* Select the text field */
-  copyText.select();
-  copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+  let temp = document.createElement('textarea');
+  let text = copyText.innerText;
+  document.body.append(temp);
+   temp.textContent = text;
+  temp.select();
+  temp.setSelectionRange(0, 99999); /*For mobile devices*/
 
   /* Copy the text inside the text field */
   document.execCommand("copy");
   /* Alert the copied text */
-  alert("Copied the text: " + copyText.value);
+  temp.remove();
+  alert("Copied to clipboard!");
 }
-
 
 let $ = document.querySelector.bind(document);
 function getFormValues(formEl) {
@@ -39,12 +44,13 @@ Node.prototype.on = function (ev, cb) {
 
 let form = $("form");
 let output = $("pre");
-let clipBtn = $('#copyClip');
-clipBtn.on('click', _ => {
-   copyToClipboard(output);
-})
+let clipBtn = $("#copyClip");
+clipBtn.on("click", (_) => {
+  copyToClipboard(output);
+});
 form.on("submit", (e) => {
   e.preventDefault();
   let string = hypers(getFormValues(e.target));
   output.innerText = string;
+  copyToClipboard(output);
 });
